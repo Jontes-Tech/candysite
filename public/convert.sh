9b87de26c4d65f719d4c14e6b7a464ee076e0eec
Jonatan Holmgren <jonatan@jontes.page>
2023-09-13 21:33:33 +0200

    MORE content

--- public/convert.sh
+++ public/convert.sh
@@ -1,5 +1,18 @@
-#!/bin/bash
-cd images
-for file in *.png; do
-  cwebp -q 80 "$file" -o "${file%.png}.webp"
-done
+# Grab last from last.txt, and add 1
+last=$(cat last.txt)
+this=$(($last + 1))
+
+url=$1
+filename=$(echo $url | sed -e 's/.*\///g')
+slug=$(echo $filename | sed -e 's/\..*//g')
+
+echo $slug
+wget -O "images/$this"_"$filename" "$url" -q
+
+echo "---" > "../src/projects/$slug.md"
+echo "title: $slug" >> "../src/projects/$slug.md"
+echo "date: $(date +%Y-%m-%d)" >> "../src/projects/$slug.md"
+echo "number: $this" >> "../src/projects/$slug.md"
+echo "---" >> "../src/projects/$slug.md"
+
+echo $this > last.txt
